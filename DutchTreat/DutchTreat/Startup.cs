de5b1d0e116ec769ee8e0e1e13250a7a61a1b9c8
Enache-Ionut <enachestoianionut@gmail.com>
2019-11-09 18:14:29 +0200

    send a post form with the contact information

--- DutchTreat/DutchTreat/Startup.cs
+++ DutchTreat/DutchTreat/Startup.cs
@@ -22,14 +22,16 @@ namespace DutchTreat
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-      if(env.IsEnvironment("Development"))
+      if(env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
       }
       else
       {
+        app.UseExceptionHandler("/error");
         // add error page
       }
+
       app.UseStaticFiles();
       app.UseNodeModules();
 

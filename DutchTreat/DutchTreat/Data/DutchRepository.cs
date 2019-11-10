2f145c18848880c462bb7fbeec01bb86d0f6c34c
Enache-Ionut <enachestoianionut@gmail.com>
2019-11-10 09:33:10 +0200

    log errors when something goes wrong in the repositorty

--- DutchTreat/DutchTreat/Data/DutchRepository.cs
+++ DutchTreat/DutchTreat/Data/DutchRepository.cs
@@ -1,4 +1,5 @@
 ﻿using DutchTreat.Data.Entities;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,30 +10,59 @@ namespace DutchTreat.Data
   public class DutchRepository : IDutchRepository
   {
     private readonly DutchContext context;
+    private readonly ILogger<DutchRepository> logger;
 
-    public DutchRepository(DutchContext context)
+    public DutchRepository(DutchContext context, ILogger<DutchRepository> logger)
     {
       this.context = context;
+      this.logger = logger;
     }
 
     public IEnumerable<Product> GetAllProducts()
     {
-      return context.Products
-        .OrderBy(p => p.Title)
-        .ToList();
+      try
+      {
+        logger.LogInformation("GetAllProducts was called");
+
+        return context.Products
+          .OrderBy(p => p.Title)
+          .ToList();
+      }
+      catch (Exception ex)
+      {
+        logger.LogError($"Failed to get all products: {ex}");
+        return null;
+      }
     }
 
     public IEnumerable<Product> GetProductsByCategory(string category)
     {
-      return context.Products
+      try
+      {
+        return context.Products
         .Where(p => p.Category == category)
         .ToList();
+      }
+      catch (Exception ex)
+      {
+        logger.LogError($"Failed to get products by category : {ex}");
+        return null;
+      }
     }
 
     public bool SaveAll()
     {
-      return context.SaveChanges() > 0;
+      try
+      {
+        return context.SaveChanges() > 0;
+      }
+      catch (Exception ex)
+      {
+        logger.LogError($"Failed to save the changes: {ex}");
+        return false;
+      }
     }
 
   }
+
 }

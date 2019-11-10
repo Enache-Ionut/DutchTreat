f62bba4f03530e88a1ba2508cd6948eb71fde14f
Enache-Ionut <enachestoianionut@gmail.com>
2019-11-10 13:22:00 +0200

    build api for get products

--- DutchTreat/DutchTreat/Startup.cs
+++ DutchTreat/DutchTreat/Startup.cs
@@ -7,6 +7,7 @@ using DutchTreat.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -39,7 +40,8 @@ namespace DutchTreat
 
       services.AddScoped<IDutchRepository, DutchRepository>();
 
-      services.AddMvc();
+      services.AddMvc()
+        .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
